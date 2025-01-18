45302bb095c5186008cc430fa9351b834967cd2d
Max Zarev <maksimelizarev@gmail.com>
2025-01-18 18:32:56 +0700

    домашка по взаимодействию с расширениями PW

--- Ads + Python/PW - работа с расширениями (метамаск и другие)/решения/homework.py
+++ Ads + Python/PW - работа с расширениями (метамаск и другие)/решения/homework.py
@@ -3,21 +3,93 @@
 """
 Задание 1 - easy
 
+Напишите скрипт, который запускает браузер,
+открывает расширение метамаска, авторизуется в кошельке,
+выбирает сеть Linea для работы.
+
 """
+password = '12345678'
+page = context.new_page()
+page.goto('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html')
+page.get_by_test_id('unlock-password').fill(password)
+page.get_by_test_id('unlock-submit').click()
 
 # код пишем тут
 
 """
 Задание 2  - medium
 
+Напишите функцию select_chain, которая принимает на вход:
+- страницу браузера
+- название сети
+
+Функция должна выбирать сеть в кошельке метамаск.
+
+
 """
+from playwright.sync_api import Page
+import time
+
+
+def select_chain(page: Page, chain: str) -> None:
+    """
+    Выбирает сеть в metamask. Если сеть не найдена, добавляет новую из параметра chain
+    :param chain: наименование сети
+    :return: None
+    """
+    page.goto('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html')
+    page.get_by_test_id("network-display").wait_for(timeout=5000, state='visible')
+    chain_button = page.get_by_test_id("network-display")
+    if chain == chain_button.inner_text():
+        return
+
+    chain_button.click()
+    time.sleep(2)
+    enabled_networks = page.locator('div[data-rbd-droppable-id="characters"]')
+    if enabled_networks.get_by_text(chain, exact=True).count():
+        enabled_networks.get_by_text(chain, exact=True).click()
+    else:
+        raise ValueError(f"Сеть {chain} не найдена")
 
 # код пишем тут
 
 """
 Задание 3 - hard
+Напишите функцию set_chain, которая принимает на вход :
+- страницу браузера
+- название сети
+- словарь с параметрами сети: rpc, chain_id, native_token
 
+Функция должна создавать сеть, если ее нет в списке сетей, и выбирать ее.
 """
+
+
+def set_chain(page: Page, chain_name: str, chain: dict) -> None:
+    """
+    Добавляет новую сеть в metamask. Берет параметры из объекта Chain.
+    :param chain: объект сети
+    :return: None
+    """
+    page.goto( "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#settings/networks/add-network")
+    time.sleep(2)
+    try:
+        select_chain(page, chain_name)
+        return
+    except ValueError:
+        page.get_by_test_id('network-form-network-name').wait_for(timeout=5000, state='visible')
+        page.get_by_test_id('network-form-network-name').fill(chain_name)
+        page.get_by_test_id('test-add-rpc-drop-down').click()
+        page.get_by_role('button', name='Add RPC URL').click()
+        page.get_by_test_id('rpc-url-input-test').fill(chain['rpc'])
+        page.get_by_role('button', name='Add URL').click()
+        if page.get_by_test_id('network-form-chain-id-error').count():
+            raise Exception(
+                f"Error: metamask не принимает rpc {chain['rpc']}, попробуйте другой")
+        page.get_by_test_id('network-form-chain-id').fill(str(chain['chain_id']))
+        page.get_by_test_id('network-form-ticker-input').fill(chain['native_token'])
+        page.get_by_role('button', name='Save').click()
+
+    select_chain(page, chain_name)
 
 # код пишем тут
 
--- Ads + Python/PW - работа с расширениями (метамаск и другие)/файлы урока/main.py
+++ Ads + Python/PW - работа с расширениями (метамаск и другие)/файлы урока/main.py
@@ -1,6 +1,15 @@
-import requests
-from playwright.sync_api import sync_playwright
 import random
+import time
+from pprint import pprint
+
+import requests
+from playwright.sync_api import sync_playwright, Page
+
+from playwright.sync_api import sync_playwright
+
+
+# Используйте идентификатор вашего расширения и путь к всплывающему окну
+
 
 def open_browser(profile_number: int) -> dict:
     params = dict(serial_number=profile_number)
@@ -9,79 +18,49 @@ def open_browser(profile_number: int) -> dict:
     data = response.json()
     return data
 
+
+def close_browser(profile_number: int) -> dict:
+    params = dict(serial_number=profile_number)
+    url = "http://local.adspower.net:50325/api/v1/browser/stop"
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
+
 def main():
     profile_data = open_browser(949)
     url_connect = profile_data.get('data').get('ws').get('puppeteer')
+    time.sleep(3)
     pw = sync_playwright().start()
-    browser = pw.chromium.connect_over_cdp(url_connect, slow_mo=1000)
+    browser = pw.chromium.connect_over_cdp(url_connect, slow_mo=random.randint(800, 1200))
     if not browser.is_connected():
         print("Browser not connected")
         exit(1)
     context = browser.contexts[0]
-    page = context.pages[0]
+    page = context.new_page()
+    page.goto('https://pancakeswap.finance/')
+    page.get_by_role('button', name='Connect').first.click()
+    with context.expect_page() as page_catcher:
+        page.get_by_text('Metamask').first.click()
+    metamask_page = page_catcher.value
+    metamask_page.wait_for_load_state(state='load')
+    metamask_page.get_by_test_id('confirm-btn').click()
 
-    page.set_default_timeout(5000)
-    page.goto('https://pancakeswap.finance/swap?chain=polygonZkEVM', timeout=30000)
+    # открывает те расширения, которые нужны для работы
+    # настройка расширений для конкретной работы
+    # если работа с кошельком:
+     # авторизация в кошельке
+     # настройка кошелька для работы BSC
+     # меняем счет в кошельке на нужный
+    # открываем сайт
+    # подключаем кошелек к сайту
+    # делаем активность
+    # Меняем сеть в кошельке
+    # подключаем кошелек к другому сайту
+    # выберу счет 1
 
-    # поиск элементов с помощью xpath
-    button = page.locator('//button[@data-dd-action-name="Select currency"]').first
-    button.click()
-
-    # поиск элементов с помощью css
-    button = page.locator('button[data-dd-action-name="Select currency"]').first
-    button.click()
-
-    # поиск элементов по классу или id
-    element = page.locator('.class-value')
-    element = page.locator('#id-value')
-
-    # поиск по тегу + класс/id
-    element = page.locator('div.class-value')
-    element = page.locator('div#id-value')
-
-    # поиск по видимым элементам
-    element = page.locator('button:visible')
-
-    # поиск по атрибуту data-testid
-    element = page.get_by_test_id('test-id')
-
-    # поиск по тексту
-    element = page.get_by_text('Connect Wallet', exact=True)
-
-    # поиск по title
-    element = page.get_by_title('title-value')
-
-    # поиск по placeholder
-    element = page.get_by_placeholder('placeholder-value')
-
-    # поиск по label
-    element = page.get_by_label('label-value')
-
-    # поиск по alt картинки
-    image = page.get_by_alt_text('alt-value')
-
-    # поиск по role
-    element = page.get_by_role('button', name='text', exact=True)
-
-    # поиск внутри ранее найденных локаторов
-    element = page.get_by_role('dialog').get_by_text('ETH', exact=True).first
-
-    # вторичный поиск элементов внутри ранее найденных элементов
-    elements = element.locator('div')
-    element = elements.get_by_text('text-value', exact=True).first
-
-    # поиск по двум условиям оператор И
-    element = element.locator('div').and_(locator=page.locator('button'))
-
-    # поиск по двум условиям оператор ИЛИ
-    element = element.locator('div').or_(locator=page.locator('button'))
-
-    # фильтрация ранее найденных элементов
-    elements = element.locator('div')
-    element = elements.filter(has_text='text-value').first
 
     pass
-
 
 
 if __name__ == '__main__':
