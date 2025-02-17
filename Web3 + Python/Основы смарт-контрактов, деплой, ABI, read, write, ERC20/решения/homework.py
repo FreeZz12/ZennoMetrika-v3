a6af866fbeaa446cd7347ccb3a1c21227351de49
Max Zarev <maksimelizarev@gmail.com>
2025-02-17 18:47:09 +0700

    домашка по контрактам

--- Web3 + Python/Основы смарт-контрактов, деплой, ABI, read, write, ERC20/решения/homework.py
+++ Web3 + Python/Основы смарт-контрактов, деплой, ABI, read, write, ERC20/решения/homework.py
@@ -14,6 +14,22 @@
 
 """
 
+import requests
+
+def get_balance(rpc_url: str, contract_address: str, address: str, decimals: int) -> float:
+    response = requests.post(rpc_url, json={
+        "jsonrpc": "2.0",
+        "method": "eth_call",
+        "params": [{
+            "to": contract_address,
+            "data": f"0x70a08231000000000000000000000000{address[2:].lower()}"
+        }, "latest"],
+        "id": 1
+    })
+
+    balance = int(response.json()['result'], 16)
+    return balance / 10 ** decimals
+
 # код пишем тут
 
 """
@@ -26,6 +42,11 @@
 Функция должна работать только. если вызывающий - владелец контракта.
 
 """
+
+# файл с контрактом в файле ZarevCoin.sol
+
+# контракт https://sepolia.etherscan.io/address/0x5ca775f0065ea685945f2a665d03a9eadac0192c
+
 
 # код пишем тут
 
